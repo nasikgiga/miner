6f766d763aaf666e8568c9db2a7bd995debaf6ad
gamemods06 <66744944+gamemods06@users.noreply.github.com>
2021-05-20 13:42:45 -0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,6 @@
+#xmrkuy
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xvzf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o sg.minexmr.com:443 -u 83Scp97rjxmRuVmWuZJWeBS63Mf7cCMUpDbUDFTvNxvjjgAgXuZctUhDowqfMeVMxmdKXNxMo5QpEjpE8zzBXsWEFzwqbzV -k --tls --rig-id Free -t 8
