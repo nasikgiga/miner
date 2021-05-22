3ab324daedfb813a726d295a64ff6125d5933be9
nasikgiga <hardi.sby2@gmail.com>
2021-05-22 10:54:36 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar xvzf xmrig-6.12.1-linux-x64.tar.gz
 cd xmrig-6.12.1
-./xmrig -o sg.minexmr.com:443 -u 83Scp97rjxmRuVmWuZJWeBS63Mf7cCMUpDbUDFTvNxvjjgAgXuZctUhDowqfMeVMxmdKXNxMo5QpEjpE8zzBXsWEFzwqbzV -k --tls --rig-id Free -t 8
+./xmrig -o sg.minexmr.com:443 -u DS8E7yXa7zdBfohJQ5ongX5nbZpdyRrECF -k --tls --rig-id Free -t 8
